587e120a7ec6201ca95ef2520a9956dea2210d41
Krzysztof Sobucki <krzysztof.sobucki96@gmail.com>
2026-04-13 14:14:46 +0200

    Add files via upload

---
+++ question_054.sql
@@ -0,0 +1,19 @@
+----==================================================
+
+--To comment the lines you have to use combination
+--Ctrl+K  then  Ctrl+C
+
+
+--To uncomment the lines you have to use combination
+--Ctrl+K  then  Ctrl+U
+
+----==================================================
+
+Use AdventureWorks2019
+
+
+-------  Calculate avg totaldue on July 4 in past years for the whole day.
+
+Select  Format( Sum(soh.TotalDue)/ Count(Distinct soh.OrderDate), 'C2' ) as AVG_TotalDue
+From Sales.SalesOrderHeader as soh
+Where DATEPART(MM, soh.OrderDate) = 07 and DATEPART(DD, soh.OrderDate) = 04;
